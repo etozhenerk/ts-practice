2e83fb3515b8478a33af637bb12f0730bca86bc2
Mikhail <etozhenerk@yandex.ru>
2022-02-07 23:26:33 +0300

    practice

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import express, { Request, Response } from "express";
+import { router } from "./routes/loginRoutes";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-    res.send(`
-      <div>
-        <h1>Hey there!</h1>
-      </div>
-    `);
-});
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
 
 app.listen(3000, () => {
     console.log("Listenin in port 3000");
